ad6078d0bb16f207269d1ba4ad0c016473e5d4fa
mohamadalik92 <mohamadalikashakesh92@gmail.com>
2025-02-15 01:45:39 +0200

    Added Description

---
+++ example2.py
@@ -0,0 +1,9 @@
+"""
+This Program asks the user to enter a number and checks if even or odd.
+"""
+
+num=int(input("Enter a number: "))
+if(num%2 == 0):
+    print("The Number is Even")
+else:
+    print("The Number is Odd")
