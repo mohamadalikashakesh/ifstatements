f7313e94c51fe5d939b94f29b5506d345957ea07
mohamadalik92 <mohamadalikashakesh92@gmail.com>
2025-02-15 01:40:21 +0200

    Fixed names

---
+++ example1.py
@@ -0,0 +1,15 @@
+"""
+This program asks the user for his age then detects ticket cost according to age.
+"""
+
+ticketcost = 0
+age = int(input("Enter your age: "))
+if age >0 and age <=5 :
+    ticketcost = 0
+elif age >5 and age <=10 :
+    ticketcost = 5
+elif age >10 and age <=20:
+    ticketcost = 10
+elif age >20 and age <= 100:
+    ticketcost = 20
+print(f"The ticket price is {ticketcost}$")
