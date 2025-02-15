39580f39bf01f64321090ea51346ef0cb478989d
mohamadalik92 <mohamadalikashakesh92@gmail.com>
2025-02-15 23:40:33 +0200

    added loop

--- example2.py
+++ example2.py
@@ -2,8 +2,12 @@
 This Program asks the user to enter a number and checks if even or odd.
 """
 
-num=int(input("Enter a number: "))
-if(num%2 == 0):
-    print("The Number is Even")
-else:
-    print("The Number is Odd")
+programrun = "y"
+while programrun == "y":
+    num=int(input("Enter a number: "))
+    if(num%2 == 0):
+        print("The Number is Even")
+    else:
+        print("The Number is Odd")
+    print("Enter y to Continue n to stop")
+    programrun = input()
